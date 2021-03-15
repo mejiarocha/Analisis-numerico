f22d4ffc03d02b6166f3d0ad4d360c2b86b047b9
mejiarocha <70533112+mejiarocha@users.noreply.github.com>
2021-03-14 22:40:58 -0500

    interseccion en las dos curvas

--- newton-rapshon.R
+++ newton-rapshon.R
@@ -3,53 +3,43 @@ newton_rapshon = function( valor,tol){
   aux=valor
   num=0
   derivada=0
-  #####
+ 
   plot(x = 1,                 
        xlab = "X", 
        ylab = "Resultado",
-       xlim = c(0, 3), 
-       ylim = c(0, 3),
+       xlim = c(-3, 4), 
+       ylim = c(-3, 4),
        main = "Newton Rapshon",
        type = "n")
   
-  #####
 
   repeat{
-    resultado=aux-((cos(aux)^2-aux^2)/(-2*aux-2*cos(aux)*sin(aux)))
-    #resultado=aux-((cos(aux)-aux)/(sin(aux)+1))
-    #resultado=aux-((aux^3 -aux-1)/(3*aux^2 -1))
-    
-    
+    resultado=aux-((sqrt(19)/sqrt(aux)-(10-aux^2)/aux)/(1+10/aux^2-sqrt(19)/((2*aux)^(3/2))))
     num=num+1
-    funcion=cos(aux)^2-aux^2
-    #funcion=cos(aux)-aux
-    #funcion=aux^3 -aux-1
+    funcion=sqrt((19)/sqrt(x))-((10-x^2)/x)
     if(funcion==0){
       break;
     }else{
-      derivada=-2*aux-2*cos(aux)*sin(aux)
-      #derivada=sin(aux)+1
-      #derivada=3*aux^2 -1
+      derivada=1+10/aux^2-sqrt(19)/((2*aux)^(3/2))
       if(derivada==0)
         break;
     }
-    if(resultado < 10^(tol)){
+    if(resultado < 2^(tol)){
       break;
     }else{
-      cat ("Resultado:",resultado, "valor de x",aux,"valor de f(x)",funcion,"valor f'(x)",derivada,"\n")
-      #cat ("Resultado:",resultado, "valor de x",aux,"\n")
+      cat ("Iteracion: ",num,"Resultado:\n\n")
+      print(resultado,50)
+            ## "valor de x",aux,"valor de f(x)",funcion,"valor f'(x)",derivada,"\n\n")
       points(x = aux, y = resultado)
+      if(num>10)
+         break;
     }
-    #if(num>100)
-     # break;
+    
     aux=resultado
   }
 }
-# 10???8; 10???16; 10???32; 10???56;
-x0 = 3
-tol=-8
-#f x^3-x-1
-#f' 3x^2-1 
+x0 = mpfr(3,50)
+tol=-16
 newton_rapshon (x0,tol)
 
 
